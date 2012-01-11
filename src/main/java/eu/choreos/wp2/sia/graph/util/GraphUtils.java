d88cc81b7f2e27298c91bc13ffd5238d9b23fab9
Guilherme Nogueira <guilherme@nirev.org>
2012-01-11 02:25:49 -0200

    correction transitive closure

--- src/main/java/eu/choreos/wp2/sia/graph/util/GraphUtils.java
+++ src/main/java/eu/choreos/wp2/sia/graph/util/GraphUtils.java
@@ -20,31 +20,11 @@ public abstract class GraphUtils{
 				new DirectedSparseGraph<Vertex, Edge>();
 		
 		for(Vertex v: graph.getVertices()){
-			//_computeTransitiveClosure(graph, transitiveClosure, v, v);
 			traverse(graph, transitiveClosure, v);
 		}
 		
 		return transitiveClosure;
 	}
-
-	/*DFS recursivo
-	private static void _computeTransitiveClosure(DirectedGraph<Vertex, Edge> graph, 
-			DirectedGraph<Vertex, Edge> transitiveClosure, Vertex v, Vertex w) {
-		
-		if (!v.equals(w)){
-			SimpleEdge simpleEdge = new SimpleEdge(
-					v.toString() + "," + w.toString());
-			
-			transitiveClosure.addEdge(simpleEdge, v, w);	
-		}
-		
-		for (Vertex suc: graph.getSuccessors(w)){			
-			if(transitiveClosure.findEdge(v, suc) == null){
-				_computeTransitiveClosure(graph, transitiveClosure, v, suc);
-			}
-		}
-	}	
-	*/
 	
 	public static void traverse(DirectedGraph<Vertex, Edge> graph, 
 			DirectedGraph<Vertex, Edge> transitiveClosure, Vertex root){
